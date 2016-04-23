a67c84dabc72c2ccb5f111271f5f1c620f00c68e
Kevin Chen <kevnchn@gmail.com>
2016-04-23 14:33:34 -0400

    (feat) Build out controller for new homepage

--- client/homepage/homeCtrl.js
+++ client/homepage/homeCtrl.js
@@ -1,6 +1,31 @@
 angular.module('roadtrippin.home', [])
   .controller('homeController', function($scope, mapFactory) {
+    $scope.input = {
+      start: '',
+      end: '',
+      inviteFields: ['']
+    };
+    $scope.isTripsClosed = false;
+    $scope.isCreateClosed = false;
 
+    mapFactory.locationAutoComplete('start', function(address) {
+      $scope.input.start = address;
+    });
+    mapFactory.locationAutoComplete('end', function(address) {
+      $scope.input.end = address;
+    });
+
+    $scope.createTrip = function() {
+      console.log($scope.input);
+    };
+
+    $scope.addInviteField = function() {
+      $scope.input.inviteFields.push('');
+    };
+
+    $scope.removeInviteField = function() {
+      $scope.input.inviteFields.pop();
+    };
 
     $scope.signout = function () {
       mapFactory.signout();
