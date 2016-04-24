355182fac6216753e39faaf5f484554239c2c15e
Rocky Murdoch <rockysta009@gmail.com>
2016-04-24 16:55:06 -0700

    (style) Add style for chat window

--- client/chat/chatCtrl.js
+++ client/chat/chatCtrl.js
@@ -3,7 +3,23 @@ var app = angular.module('chat', []);
 app.factory('socket', function() {
     var socket = io.connect();
     return socket;
-  });
+});
+
+app.factory('chatFact', function($http) {
+  return {
+    getChat: function(trip_id) {
+      return $http({
+          method: 'POST',
+          url: '/chat',
+          data: { trip_id: "571acec0cb131271a6f8cd07"/*trip_id*/ }
+        })
+        .then(function(res) {
+          return res.data;
+        });
+    }
+  };
+});
+
 
 app.directive('scrollBottom', function () {
   return {
@@ -34,12 +50,12 @@ app.directive('a', function() {
    };
 });
 
-app.controller('chatController', function($scope, socket) {
+app.controller('chatController', function($scope, socket, chatFact) {
     $scope.messages = [];
 
     $scope.sendMsg  = function($event) {
       $scope.data = {
-        // trip_id: trip_id
+        trip_id: trip_id
         username: 'rocky',
         message: $scope.data.message
       };
@@ -54,10 +70,11 @@ app.controller('chatController', function($scope, socket) {
       $scope.$digest();
     });
 
-    console.log($scope.messages);
-    console.log($scope.data);
-
-    $scope.getChat = function() {
-      // TO DO - get chat upon load
-    }
+    $scope.getChat = function(trip_id) {
+        chatFact.getChat(trip_id)            
+          .then(function(messages) {
+            $scope.messages = messages;
+            console.log($scope.messages)
+          });
+    };
 });
